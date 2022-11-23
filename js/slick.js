574d8dbe30f6e6d4375e1a26c68bb2dadbd36ab8
levy52 <radeklewicki52@gmail.com>
2022-11-23 12:53:56 +0100

    add section contact

--- js/slick.js
+++ js/slick.js
@@ -1,4 +1,7 @@
-$(document).ready(function(){
-    $('.carousel').slick({
-    });
-  });
+$(document).ready(function () {
+	$('.carousel').slick({
+		autoplaySpeed: 3000,
+		nextArrow: '<i class="fa-solid fa-caret-right"></i>',
+		prevArrow: '<i class="fa-solid fa-caret-left"></i>',
+	})
+})
